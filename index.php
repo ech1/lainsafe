ad984d8b985c81350e9d96fd6078b36bda35b72a
ech1 <ech1@nowhere.com>
2020-11-13 21:52:44 +0100

    main files

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FileSafe</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="blacc" style="background-image: url('wallpaper.png'); "></br></br></br>
+<div class="greeting0"><h1>PHP LainSafe</h1></div>
+</br></br>
+<img src="logo.png">
+<h1>Max FileSize: 100mb</h1></br></br></br></br>
+
+<form action="upload.php" method="POST" enctype="multipart/form-data">
+    <input  type="file" name="file">
+    <p>Drag your files here or click in this area. </p>
+    <button  type="submit" name="submit">Upload</button>
+</form>
+
+
+<?php
+print_r(isset($_POST['submit']));
+?>
+
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</br></br></br></br></br></br></br></br></br></br>
+</div>
+</body>
+
+</html>
---
+++ upload.php
@@ -0,0 +1,67 @@
+
+<?php
+// Program to display URL of current page.
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+    $link = "https";
+else
+    $link = "http";
+// Here append the common URL characters.
+$link .= "://";
+// Append the host(domain name, ip) to the URL.
+$link .= $_SERVER['HTTP_HOST'];
+// Append the requested resource location to the URL
+//$link .= $_SERVER['REQUEST_URI'];
+// Print the link
+//echo $link;
+
+if(isset($_POST['submit'])){
+    //code
+    $file=$_FILES['file'];
+    //in index.php we named the file "file"
+    //print_r($file);
+    $fileName=$_FILES['file']['name'];
+    $fileTmpName=$_FILES['file']['tmp_name'];
+    $fileSize=$_FILES['file']['size'];
+    $fileError=$_FILES['file']['error'];
+    $fileType=$_FILES['file']['type'];
+
+    //name > imagefile.jpg so we separate . and jpg
+    $fileExt = explode('.', $fileName);         //JpG
+    $fileActualExt = strtolower(end($fileExt)); //jpg
+
+    $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+    if(in_array($fileActualExt, $allowed)){
+        //if extension is in there:
+        if($fileError === 0){
+            if($fileSize < 100000000){ //less than 100mb
+                //unique id for the file uploaded (to avoid overwrites)
+
+                $fileNameNew= uniqid('', true).".".$fileActualExt;
+                //time format in microseconds, becomes unique! so : 216155646120.png
+
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName,$fileDestination);
+                //header("Location: index.php?uploadsuccess");
+
+                echo $link.'/uploads/'.$fileNameNew;
+                $filemb=$fileSize/1000000;
+                echo "</br></br>";
+                echo "filesize =".$filemb."mb";
+                echo "  </br></br>Return to <a href='index.php'>Home</a>";
+            }else{
+                echo "your file is too big!".$fileSize;
+            }
+
+        } else{
+            echo "error during upload!";
+        }
+
+    }else{
+        echo "you cant upload this filetype!";
+    }
+}
+?>
+
+
+
+
