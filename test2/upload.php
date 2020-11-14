f8286d373f242640684bd2a509ff4933ad2cf150
ech1 <ech1@nowhere.com>
2020-11-14 11:15:17 +0100

    update

--- test2/upload.php
+++ test2/upload.php
@@ -11,12 +11,12 @@
 		$fileActualExt = strtolower(end($fileExt));
 		$not_allowed = array('sh','php');
 		
-		if($_FILES['files']['error'][$key] === 0 && $filesize < 100000000){
+		if($_FILES['files']['error'][$key] === 0 && $filesize < 100000000 && preg_match('/(<\?php\s)/',$input) == false && $fileActualExt, $not_allowed) == false ){
 			$temp = $_FILES['files']['tmp_name'][$key];
 			$ext = strtolower(end(explode('.', $name)));
 			$file = uniqid('', true).time().'.'.$ext;
 			$input = file_get_contents($_FILES['files']['tmp_name']);
-			if(in_array($fileActualExt, $not_allowed) == false && move_uploaded_file($temp, '../uploads/'.$file) && preg_match('/(<\?php\s)/',$input) == false) {
+			if(move_uploaded_file($temp, '../uploads/'.$file)) {
 				$processed[] = array (
 					'name' => $name,
 					'file' => $file,
