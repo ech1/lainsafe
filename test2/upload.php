4548d41d073bc5a36af97354dee3191ec754dadb
ech1 <ech1@nowhere.com>
2020-11-14 10:57:09 +0100

    update

--- test2/upload.php
+++ test2/upload.php
@@ -7,11 +7,11 @@
 	foreach($_FILES['files']['name'] as $key => $name) {
 		$fileSize=$_FILES['file']['size'];
 		$not_allowed = array('sh','php');
-		if($_FILES['files']['error'][$key] === 0 && $filesize < 100000000 && !in_array($fileActualExt, $not_allowed)){
+		if($_FILES['files']['error'][$key] === 0 && $filesize < 100000000){
 			$temp = $_FILES['files']['tmp_name'][$key];
 			$ext = strtolower(end(explode('.', $name)));
 			$file = uniqid('', true).time().'.'.$ext;
-			if(in_array($ext, $allowed) && move_uploaded_file($temp, '../uploads/'.$file)) {
+			if(in_array(!in_array($fileActualExt, $not_allowed) && move_uploaded_file($temp, '../uploads/'.$file)) {
 				$processed[] = array (
 					'name' => $name,
 					'file' => $file,
